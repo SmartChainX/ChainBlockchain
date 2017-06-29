beffbd93ca4a0af1eba2e45a00f7a4a83b320fee
SmartChainX <cryptogirl21@gmail.com>
2017-06-29 20:13:56 +0800

    cpp 	implement chainxd.giga wallet

---
+++ Maternodes/ChainX-MN/rpcmasternode.cpp
@@ -0,0 +1,236 @@
+#include "activemasternode.h"
+#include "darksend.h"
+#include "init.h"
+#include "main.h"
+#include "masternode-payments.h"
+#include "masternode-sync.h"
+#include "masternodeconfig.h"
+#include "masternodeman.h"
+#include "rpcserver.h"
+#include "util.h"
+#include "utilmoneystr.h"
+
+#include <fstream>
+#include <iomanip>
+#include <univalue.h>
+
+void EnsureWalletIsUnlocked();
+
+UniValue privatesend(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() != 1)
+        throw std::runtime_error(
+            "privatesend \"command\"\n"
+            "\nArguments:\n"
+            "1. \"command\"        (string or set of strings, required) The command to execute\n"
+            "\nAvailable commands:\n"
+            "  start       - Start mixing\n"
+            "  stop        - Stop mixing\n"
+            "  reset       - Reset mixing\n"
+            );
+
+    if(params[0].get_str() == "start") {
+        {
+            LOCK(pwalletMain->cs_wallet);
+            EnsureWalletIsUnlocked();
+        }
+
+        if(fMasterNode)
+            return "Mixing is not supported from masternodes";
+
+        fEnablePrivateSend = true;
+        bool result = darkSendPool.DoAutomaticDenominating();
+        return "Mixing " + (result ? "started successfully" : ("start failed: " + darkSendPool.GetStatus() + ", will retry"));
+    }
+
+    if(params[0].get_str() == "stop") {
+        fEnablePrivateSend = false;
+        return "Mixing was stopped";
+    }
+
+    if(params[0].get_str() == "reset") {
+        darkSendPool.ResetPool();
+        return "Mixing was reset";
+    }
+
+    return "Unknown command, please see \"help privatesend\"";
+}
+
+UniValue getpoolinfo(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() != 0)
+        throw std::runtime_error(
+            "getpoolinfo\n"
+            "Returns an object containing mixing pool related information.\n");
+
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("state",             darkSendPool.GetStateString()));
+    obj.push_back(Pair("mixing_mode",       fPrivateSendMultiSession ? "multi-session" : "normal"));
+    obj.push_back(Pair("queue",             darkSendPool.GetQueueSize()));
+    obj.push_back(Pair("entries",           darkSendPool.GetEntriesCount()));
+    obj.push_back(Pair("status",            darkSendPool.GetStatus()));
+
+    if (darkSendPool.pSubmittedToMasternode) {
+        obj.push_back(Pair("outpoint",      darkSendPool.pSubmittedToMasternode->vin.prevout.ToStringShort()));
+        obj.push_back(Pair("addr",          darkSendPool.pSubmittedToMasternode->addr.ToString()));
+    }
+
+    if (pwalletMain) {
+        obj.push_back(Pair("keys_left",     pwalletMain->nKeysLeftSinceAutoBackup));
+        obj.push_back(Pair("warnings",      pwalletMain->nKeysLeftSinceAutoBackup < PRIVATESEND_KEYS_THRESHOLD_WARNING
+                                                ? "WARNING: keypool is almost depleted!" : ""));
+    }
+
+    return obj;
+}
+
+
+UniValue masternode(const UniValue& params, bool fHelp)
+{
+    std::string strCommand;
+    if (params.size() >= 1) {
+        strCommand = params[0].get_str();
+    }
+
+    if (strCommand == "start-many")
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "DEPRECATED, please use start-all instead");
+
+    if (fHelp  ||
+        (strCommand != "start" && strCommand != "start-alias" && strCommand != "start-all" && strCommand != "start-missing" &&
+         strCommand != "start-disabled" && strCommand != "list" && strCommand != "list-conf" && strCommand != "count" &&
+         strCommand != "debug" && strCommand != "current" && strCommand != "winner" && strCommand != "winners" && strCommand != "genkey" &&
+         strCommand != "connect" && strCommand != "outputs" && strCommand != "status"))
+            throw std::runtime_error(
+                "masternode \"command\"...\n"
+                "Set of commands to execute masternode related actions\n"
+                "\nArguments:\n"
+                "1. \"command\"        (string or set of strings, required) The command to execute\n"
+                "\nAvailable commands:\n"
+                "  count        - Print number of all known masternodes (optional: 'ps', 'enabled', 'all', 'qualify')\n"
+                "  current      - Print info on current masternode winner to be paid the next block (calculated locally)\n"
+                "  debug        - Print masternode status\n"
+                "  genkey       - Generate new masternodeprivkey\n"
+                "  outputs      - Print masternode compatible outputs\n"
+                "  start        - Start local Hot masternode configured in dash.conf\n"
+                "  start-alias  - Start single remote masternode by assigned alias configured in masternode.conf\n"
+                "  start-<mode> - Start remote masternodes configured in masternode.conf (<mode>: 'all', 'missing', 'disabled')\n"
+                "  status       - Print masternode status information\n"
+                "  list         - Print list of all known masternodes (see masternodelist for more info)\n"
+                "  list-conf    - Print masternode.conf in JSON format\n"
+                "  winner       - Print info on next masternode winner to vote for\n"
+                "  winners      - Print list of masternode winners\n"
+                );
+
+    if (strCommand == "list")
+    {
+        UniValue newParams(UniValue::VARR);
+        // forward params but skip "list"
+        for (unsigned int i = 1; i < params.size(); i++) {
+            newParams.push_back(params[i]);
+        }
+        return masternodelist(newParams, fHelp);
+    }
+
+    if(strCommand == "connect")
+    {
+        if (params.size() < 2)
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "Masternode address required");
+
+        std::string strAddress = params[1].get_str();
+
+        CService addr = CService(strAddress);
+
+        CNode *pnode = ConnectNode((CAddress)addr, NULL);
+        if(!pnode)
+            throw JSONRPCError(RPC_INTERNAL_ERROR, strprintf("Couldn't connect to masternode %s", strAddress));
+
+        return "successfully connected";
+    }
+
+    if (strCommand == "count")
+    {
+        if (params.size() > 2)
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "Too many parameters");
+
+        if (params.size() == 1)
+            return mnodeman.size();
+
+        std::string strMode = params[1].get_str();
+
+        if (strMode == "ps")
+            return mnodeman.CountEnabled(MIN_PRIVATESEND_PEER_PROTO_VERSION);
+
+        if (strMode == "enabled")
+            return mnodeman.CountEnabled();
+
+        int nCount;
+        mnodeman.GetNextMasternodeInQueueForPayment(true, nCount);
+
+        if (strMode == "qualify")
+            return nCount;
+
+        if (strMode == "all")
+            return strprintf("Total: %d (PS Compatible: %d / Enabled: %d / Qualify: %d)",
+                mnodeman.size(), mnodeman.CountEnabled(MIN_PRIVATESEND_PEER_PROTO_VERSION),
+                mnodeman.CountEnabled(), nCount);
+    }
+
+    if (strCommand == "current" || strCommand == "winner")
+    {
+        int nCount;
+        int nHeight;
+        CMasternode* winner = NULL;
+        {
+            LOCK(cs_main);
+            nHeight = chainActive.Height() + (strCommand == "current" ? 1 : 10);
+        }
+        mnodeman.UpdateLastPaid();
+        winner = mnodeman.GetNextMasternodeInQueueForPayment(nHeight, true, nCount);
+        if(!winner) return "unknown";
+
+        UniValue obj(UniValue::VOBJ);
+
+        obj.push_back(Pair("height",        nHeight));
+        obj.push_back(Pair("IP:port",       winner->addr.ToString()));
+        obj.push_back(Pair("protocol",      (int64_t)winner->nProtocolVersion));
+        obj.push_back(Pair("vin",           winner->vin.prevout.ToStringShort()));
+        obj.push_back(Pair("payee",         CBitcoinAddress(winner->pubKeyCollateralAddress.GetID()).ToString()));
+        obj.push_back(Pair("lastseen",      (winner->lastPing == CMasternodePing()) ? winner->sigTime :
+                                                    winner->lastPing.sigTime));
+        obj.push_back(Pair("activeseconds", (winner->lastPing == CMasternodePing()) ? 0 :
+                                                    (winner->lastPing.sigTime - winner->sigTime)));
+        return obj;
+    }
+
+    if (strCommand == "debug")
+    {
+        if(activeMasternode.nState != ACTIVE_MASTERNODE_INITIAL || !masternodeSync.IsBlockchainSynced())
+            return activeMasternode.GetStatus();
+
+        CTxIn vin;
+        CPubKey pubkey;
+        CKey key;
+
+        if(!pwalletMain || !pwalletMain->GetMasternodeVinAndKeys(vin, pubkey, key))
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "Missing masternode input, please look at the documentation for instructions on masternode creation");
+
+        return activeMasternode.GetStatus();
+    }
+
+    if (strCommand == "start")
+    {
+        if(!fMasterNode)
+            throw JSONRPCError(RPC_INTERNAL_ERROR, "You must set masternode=1 in the configuration");
+
+        {
+            LOCK(pwalletMain->cs_wallet);
+            EnsureWalletIsUnlocked();
+        }
+
+        if(activeMasternode.nState != ACTIVE_MASTERNODE_STARTED){
+            activeMasternode.nState = ACTIVE_MASTERNODE_INITIAL; // TODO: consider better way
+            activeMasternode.ManageState();
+        }
+
+        return activeMasternode.GetStatus();
+}
