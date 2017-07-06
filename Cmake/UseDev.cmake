d0737bdb25d3c2418a4de985328661e0b99c21e7
Klaus Moeller <pandeyishan6666@gmail.com>
2017-07-06 18:29:12 +0800

    Added Libraries

---
+++ Cmake/UseDev.cmake
@@ -0,0 +1,61 @@
+function(chx_apply TARGET REQUIRED SUBMODULE)
+
+
+
+	# Base is where all dependencies for devcore are
+
+	if (${SUBMODULE} STREQUAL "base")
+
+		# if it's chainx source dir, always build BuildInfo.h before
+
+		chx_use(${TARGET} ${REQUIRED} Dev::buildinfo)
+
+
+
+		target_include_directories(${TARGET} SYSTEM PUBLIC ${Boost_INCLUDE_DIRS})
+
+		target_link_libraries(${TARGET} ${Boost_THREAD_LIBRARIES})
+
+		target_link_libraries(${TARGET} ${Boost_RANDOM_LIBRARIES})
+
+		target_link_libraries(${TARGET} ${Boost_FILESYSTEM_LIBRARIES})
+
+		target_link_libraries(${TARGET} ${Boost_SYSTEM_LIBRARIES})
+
+		target_link_libraries(${TARGET} ${Boost_REGEX_LIBRARIES})
+
+
+
+		if (DEFINED MSVC)
+
+			target_link_libraries(${TARGET} ${Boost_CHRONO_LIBRARIES})
+
+			target_link_libraries(${TARGET} ${Boost_DATE_TIME_LIBRARIES})
+
+		endif()
+
+
+
+		if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
+
+			target_link_libraries(${TARGET} pthread)
+
+		endif()
+
+
+
+	endif()
+
+
+
+	if (${SUBMODULE} STREQUAL "soldevcore")
+
+		chx_use(${TARGET} ${REQUIRED} Dev::base)
+
+		target_link_libraries(${TARGET} soldevcore)
+
+	endif()
+
+
+
+endfunction()
